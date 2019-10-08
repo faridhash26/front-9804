6ffb6dd5a161e5d4fd8e97043b8c4e610c722ff1
faridhash26 <faridhash26@gmail.com>
2019-10-08 19:29:15 +0330

    create the fragmentidentifire

--- js/index.js
+++ js/index.js
@@ -18,8 +18,19 @@ $(document).ready(function () {
 //***********about us   *************
 
 
-// $(document).ready(function () {
-//
-// });
+$(function() {
+    $('a[href]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 3000);
+                return false;
+            }
+        }
+    });
+});
 
 //***********/about us   *************
